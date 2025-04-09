955f616d0cedc3c4e4acca78b1294ed106e5b65f
AlbertV1810 <143655634+AlbertV1810@users.noreply.github.com>
2025-04-09 18:27:13 -0400

    modal behavior

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,19 @@
-"use strict";
-//# sourceMappingURL=main.js.map
+const modalTrigger = document.querySelector('#js-triggers li a[href="#"]:nth-child(2)');
+const modalPanel = document.querySelector('.modal-panel');
+
+modalTrigger.addEventListener('click', (e) => {
+  e.preventDefault();
+  modalPanel.style.display = 'block';
+});
+
+modalPanel.addEventListener('click', (e) => {
+  if (e.target === modalPanel) {
+    modalPanel.style.display = 'none';
+  }
+});
+
+document.addEventListener('keydown', (e) => {
+  if (e.key === 'Escape') {
+    modalPanel.style.display = 'none';
+  }
+});
