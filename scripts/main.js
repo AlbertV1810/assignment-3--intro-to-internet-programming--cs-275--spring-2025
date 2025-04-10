d3a8d81dc68edd038da8ce39f762c42e923b511a
AlbertV1810 <143655634+AlbertV1810@users.noreply.github.com>
2025-04-09 20:08:56 -0400

    menu

--- scripts/main.js
+++ scripts/main.js
@@ -18,3 +18,37 @@ showModalButton.addEventListener('click', (event) => {
 });
 modalPanel.addEventListener('click', hideModal);
 document.addEventListener('keydown', hideModal);
+
+const showMenuButton = document.querySelector('#js-triggers li:nth-child(1) a');
+const dropdownMenu = document.querySelector('.dropdown-menu');
+const sideTray = document.querySelector('.side-tray');
+
+const showMenu = () => {
+  const screenWidth = window.innerWidth;
+
+  if (screenWidth > 736) {
+    dropdownMenu.style.display = `block`;
+    sideTray.style.display = `none`;
+  } else {
+    sideTray.style.display = `block`;
+    dropdownMenu.style.display = `none`;
+  }
+};
+
+const hideMenu = () => {
+  dropdownMenu.style.display = `none`;
+  sideTray.style.display = `none`;
+};
+
+showMenuButton.addEventListener('click', (event) => {
+  event.preventDefault();
+  showMenu();
+});
+
+window.addEventListener('resize', () => {
+  if (window.innerWidth > 736) {
+    dropdownMenu.style.display = `none`;
+  } else {
+    sideTray.style.display = `none`;
+  }
+});
