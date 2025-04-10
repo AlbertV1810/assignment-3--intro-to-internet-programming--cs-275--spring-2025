c60c0782f8ae09effcf91f5ab327e781160053fa
AlbertV1810 <143655634+AlbertV1810@users.noreply.github.com>
2025-04-10 12:18:56 -0400

    menuedit

--- scripts/main.js
+++ scripts/main.js
@@ -18,37 +18,42 @@ showModalButton.addEventListener('click', (event) => {
 });
 modalPanel.addEventListener('click', hideModal);
 document.addEventListener('keydown', hideModal);
-
+//
 const showMenuButton = document.querySelector('#js-triggers li:nth-child(1) a');
-const dropdownMenu = document.querySelector('.dropdown-menu');
-const sideTray = document.querySelector('.side-tray');
+const navMenu = document.querySelector('nav');
 
 const showMenu = () => {
   const screenWidth = window.innerWidth;
 
   if (screenWidth > 736) {
-    dropdownMenu.style.display = `block`;
-    sideTray.style.display = `none`;
+    navMenu.style.display = `block`;
   } else {
-    sideTray.style.display = `block`;
-    dropdownMenu.style.display = `none`;
+    navMenu.style.display = `block`;
+    navMenu.classList.add('side-tray');
   }
 };
 
+
 const hideMenu = () => {
-  dropdownMenu.style.display = `none`;
-  sideTray.style.display = `none`;
+  navMenu.style.display = `none`;
+  navMenu.classList.remove('side-tray');
 };
 
 showMenuButton.addEventListener('click', (event) => {
   event.preventDefault();
-  showMenu();
+  const isVisible = navMenu.style.display === `block`;
+  if (isVisible) {
+    hideMenu();
+  } else {
+    showMenu();
+  }
 });
 
 window.addEventListener('resize', () => {
   if (window.innerWidth > 736) {
-    dropdownMenu.style.display = `none`;
+    navMenu.style.display = `none`;
+    navMenu.classList.remove('side-tray');
   } else {
-    sideTray.style.display = `none`;
+    navMenu.style.display = `none`;
   }
 });
